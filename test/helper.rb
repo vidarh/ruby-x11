86e9d85ffcf943f080fd2306593cb0628da5cfe0
Vidar Hokstad <vidar@hokstad.com>
2025-05-04 02:32:27 +0100

    Fix circular require warning for test suite

--- test/helper.rb
+++ test/helper.rb
@@ -5,8 +5,8 @@ Bundler.setup(:default, :development)
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
-require 'X11'
-
-# Fix circular dependency by loading minitest after the application code
-require 'minitest/spec'
+# Load minitest before application code to avoid circular require
 require 'minitest/autorun'
+
+# Then load application code
+require 'X11'
